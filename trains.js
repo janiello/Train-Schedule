6575d43c188fb9d9276dc028dc29a18d32089680
janiello <janiello417@gmail.com>
2019-04-27 21:41:54 -0400

    Began addding calculations. time differential equation keeps returning NaN

--- trains.js
+++ trains.js
@@ -7,18 +7,28 @@ var config = {
     storageBucket: "train-schedule-98957.appspot.com",
     messagingSenderId: "426358396482"
   };
-
 firebase.initializeApp(config);
-
 var database = firebase.database();
+// Grab input from text fields where user will enter new train info. These will be initial values
+var trainName = $("#name-input").val().trim();
+var destination = $("#destination-input").val().trim();
+var firstTrain = $("#original-input").val().trim();
+var frequency = $("#frequency-input").val().trim();
+// Convert our time input to realtime
+var firstTrainConverted = moment(firstTrain, "HH:mm").subtract(1, "years");
+console.log(firstTrainConverted);
+// Set the current time in real life (this seems like magic. Technology, man.)
+var currentTime = moment();
+console.log("Current time: " + moment(currentTime).format("HH:mm"));
+// Calculate the difference in time between when the first train left and now
+var diffTime = moment().diff(moment(firstTrainConverted), "minutes");
+console.log("How long has it been since the first train left? " + diffTime);
+// Calculate the remainder of time by dividing the difference in time by the frequency of trains
+var remainder = diffTime % frequency;
+console.log(remainder);
 // Button click event to submit new train schedule
 $("#submit").on("click", function(event) {
     event.preventDefault();
-    // Grab input from text fields where user will enter new train info
-    var trainName = $("#name-input").val().trim();
-    var destination = $("#destination-input").val().trim();
-    var firstTrain = $("#original-input").val().trim();
-    var frequency = $("#frequency-input").val().trim();
     // Create an object to temporarily store new info
     var newSchedule = {
         name: trainName,
@@ -36,11 +46,6 @@ $("#submit").on("click", function(event) {
 });
 // Function that continually reads firebase and console logs all data requested
 database.ref().on("child_added", function(childSnapshot) {
-    console.log(childSnapshot.val());
-    console.log(childSnapshot.val().name);
-    console.log(childSnapshot.val().destination);
-    console.log(childSnapshot.val().first);
-    console.log(childSnapshot.val().frequency);
     // Append the above information to the table in the html by programmaticly creating row elements
     $("#all-train-schedules").append("<tr class='new-train'><td class='train-name'> " + childSnapshot.val().name + 
     " </td><td class='train-destination'> " + childSnapshot.val().destination +  
